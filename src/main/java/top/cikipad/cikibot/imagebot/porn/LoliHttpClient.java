3b4d9b34818216fa14b764ec73ab83d9e8282e34
cikiPAD <58922174+cikiPAD@users.noreply.github.com>
2024-11-13 10:37:38 +0800

    Update LoliHttpClient.java

--- src/main/java/top/cikipad/cikibot/imagebot/porn/LoliHttpClient.java
+++ src/main/java/top/cikipad/cikibot/imagebot/porn/LoliHttpClient.java
@@ -12,6 +12,8 @@ import org.apache.http.util.EntityUtils;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.Map;
 
 
@@ -179,6 +181,29 @@ public class LoliHttpClient {
             }
         }
         return content;
+    }
+
+
+    public static boolean isLinkValid(String urlString) {
+        HttpURLConnection connection = null;
+        try {
+            URL url = new URL(urlString);
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("HEAD"); // 使用HEAD方法进行请求，避免下载内容
+            connection.setConnectTimeout(3000); // 设置连接超时时间
+            connection.setReadTimeout(3000); // 设置读取超时时间
+            int responseCode = connection.getResponseCode();
+
+            // 判断响应码是否为404
+            return (responseCode == HttpURLConnection.HTTP_OK);
+        } catch (Exception e) {
+            log.error("", e);
+            return false;
+        } finally {
+            if (connection != null) {
+                connection.disconnect(); // 断开连接
+            }
+        }
     }
 
 
